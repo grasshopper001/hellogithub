1f2476c0ac1f5f6beec9059953c19a9d5a5ba5d9
Emily <1787485473@qq.com>
2019-05-09 19:32:56 +0800

    试一下能否merge

--- app/src/main/java/grasshopper001/learn/MainActivity.java
+++ app/src/main/java/grasshopper001/learn/MainActivity.java
@@ -2,12 +2,27 @@ package grasshopper001.learn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-//can you see me? I'm branch1 XD
+/*从master看到这条的话说明merge了！
+是简单地试了下edittext.setText()这个，似乎没问题
+ */
+    EditText editText;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.name);
+        button = (Button)findViewById(R.id.change);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("sy");
+            }
+        });
     }
 }
